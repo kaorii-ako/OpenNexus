915a2d7633e669a6ae3b57407df094625f4e11b2
Tawin Tangsukson <79807@student.amnuaysilpa.ac.th>
2026-06-01 15:48:35 +0700

    fix: add missing classroom.student-submissions scope

--- backend/connectors/gmail.py
+++ backend/connectors/gmail.py
@@ -12,6 +12,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/classroom.courses.readonly",
     "https://www.googleapis.com/auth/classroom.coursework.me.readonly",
+    "https://www.googleapis.com/auth/classroom.student-submissions.me.readonly",
     "https://www.googleapis.com/auth/classroom.announcements.readonly",
 ]
 
